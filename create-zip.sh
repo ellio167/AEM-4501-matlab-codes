e5111442d2b16cc4718e29338892683308a8e03d
Ryan S. Elliott <relliott@umn.edu>
2018-01-15 08:58:46 -0600

    Updates for S18

--- create-zip.sh
+++ create-zip.sh
@@ -1,48 +1,56 @@
 #!/bin/sh
 
-cd ../
+prefix=AEM-4501-S18-matlab-code
 
-zip AEM-4501-S15-matlab-code/AEM-4501-S15-matlab-code.zip \
-    AEM-4501-S15-matlab-code/README.TXT\
-    AEM-4501-S15-matlab-code/DISTMESH_COPYRIGHT.TXT\
-    AEM-4501-S15-matlab-code/DISTMESH_FILE_LIST.TXT\
-    AEM-4501-S15-matlab-code/PD_torsion.m\
-    AEM-4501-S15-matlab-code/PD_torsion_poly.m\
-    AEM-4501-S15-matlab-code/PD_truss_modes.m\
-    AEM-4501-S15-matlab-code/PD_truss_static.m\
-    AEM-4501-S15-matlab-code/PlotTruss.m\
-    AEM-4501-S15-matlab-code/PlotTrussMode.m\
-    AEM-4501-S15-matlab-code/assemble_PD_torsion_fem.m\
-    AEM-4501-S15-matlab-code/assemble_PD_truss_fem.m\
-    AEM-4501-S15-matlab-code/assemble_bar_fem.m\
-    AEM-4501-S15-matlab-code/assemble_gmesh_bar_fem.m\
-    AEM-4501-S15-matlab-code/assemble_gmesh_beam_fem.m\
-    AEM-4501-S15-matlab-code/bar_dynamic.m\
-    AEM-4501-S15-matlab-code/bar_forced.m\
-    AEM-4501-S15-matlab-code/bar_impact.m\
-    AEM-4501-S15-matlab-code/bar_modes.m\
-    AEM-4501-S15-matlab-code/bar_static.m\
-    AEM-4501-S15-matlab-code/beam_free_vib.m\
-    AEM-4501-S15-matlab-code/beam_interpolate_results.m\
-    AEM-4501-S15-matlab-code/boundedges.m\
-    AEM-4501-S15-matlab-code/create_gmesh_for_uniform_mesh.m\
-    AEM-4501-S15-matlab-code/distmesh2d.m\
-    AEM-4501-S15-matlab-code/dpoly.m\
-    AEM-4501-S15-matlab-code/dsegment.mexa64\
-    AEM-4501-S15-matlab-code/dsegment.mexmaci64\
-    AEM-4501-S15-matlab-code/dsegment.mexw32\
-    AEM-4501-S15-matlab-code/dsegment.mexw64\
-    AEM-4501-S15-matlab-code/fixmesh.m\
-    AEM-4501-S15-matlab-code/gmesh_bar_dynamic.m\
-    AEM-4501-S15-matlab-code/gmesh_bar_modes.m\
-    AEM-4501-S15-matlab-code/gmesh_bar_static.m\
-    AEM-4501-S15-matlab-code/gmesh_beam_dynamic.m\
-    AEM-4501-S15-matlab-code/gmesh_beam_modes.m\
-    AEM-4501-S15-matlab-code/gmesh_beam_static.m\
-    AEM-4501-S15-matlab-code/huniform.m\
-    AEM-4501-S15-matlab-code/simpplot.m\
-    AEM-4501-S15-matlab-code/simpvol.m\
-    AEM-4501-S15-matlab-code/spring_mass_forced.m\
-    AEM-4501-S15-matlab-code/truss_2d_example.m\
-    AEM-4501-S15-matlab-code/truss_example.m\
-    AEM-4501-S15-matlab-code/truss_local_matrices.m\
+fileList="README.TXT\
+         DISTMESH_COPYRIGHT.TXT\
+         DISTMESH_FILE_LIST.TXT\
+         PD_torsion.m\
+         PD_torsion_poly.m\
+         PD_truss_modes.m\
+         PD_truss_static.m\
+         PlotTruss.m\
+         PlotTrussMode.m\
+         assemble_PD_torsion_fem.m\
+         assemble_PD_truss_fem.m\
+         assemble_bar_fem.m\
+         assemble_gmesh_bar_fem.m\
+         assemble_gmesh_beam_fem.m\
+         bar_dynamic.m\
+         bar_forced.m\
+         bar_impact.m\
+         bar_modes.m\
+         bar_static.m\
+         beam_free_vib.m\
+         beam_interpolate_results.m\
+         boundedges.m\
+         create_gmesh_for_uniform_mesh.m\
+         distmesh2d.m\
+         dpoly.m\
+         dsegment.mexa64\
+         dsegment.mexmaci64\
+         dsegment.mexw32\
+         dsegment.mexw64\
+         fixmesh.m\
+         gmesh_bar_dynamic.m\
+         gmesh_bar_modes.m\
+         gmesh_bar_static.m\
+         gmesh_beam_dynamic.m\
+         gmesh_beam_modes.m\
+         gmesh_beam_static.m\
+         huniform.m\
+         simpplot.m\
+         simpvol.m\
+         spring_mass_forced.m\
+         truss_2d_example.m\
+         truss_example.m\
+         truss_local_matrices.m"
+fileList=`printf "${fileList}" | sed -e 's/[[:space:]][[:space:]]*/ /g'`
+
+
+mkdir "${prefix}"
+cp ${fileList} "${prefix}"/
+
+zip -r "${prefix}.zip" "${prefix}"
+
+rm -rf "${prefix}"
